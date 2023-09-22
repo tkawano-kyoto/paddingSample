1d8213c776da880721f8b9a8bffaaf06ee5e7caa
Tamotsu Kawano <t.k.kyoto@gmail.com>
2023-09-22 19:28:21 +0900

    Initial Commit

---
+++ paddingSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  paddingSample
+//
+//  Created by 河野保 on 2023/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ paddingSample/paddingSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  paddingSampleApp.swift
+//  paddingSample
+//
+//  Created by 河野保 on 2023/09/22.
+//
+
+import SwiftUI
+
+@main
+struct paddingSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
